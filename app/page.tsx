a533b1740210c6bdca0ada2da1ec9dcc1db04794
parsa <taghizadehparsa84@gmail.com>
2023-08-14 13:49:27 +0330

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <section
+        className="flex items-center justify-start
+       flex-col lg:px-20 py-6 px-5
+        mb-16"
+      ></section>
+    </>
+  );
+};
+
+export default Home;
